21cf8e9e3098ca06b48e966aadc4f3e5dfc774d8
ecandotti <enzo.candotti16@hotmail.com>
2022-03-04 22:47:13 +0100

    init pages

---
+++ src/screens/Login.jsx
@@ -0,0 +1,38 @@
+import axios from "axios"
+import React, { useContext, useState } from "react"
+import { SafeAreaView, TextInput, Button } from "react-native"
+import { useAsyncStorage } from "@react-native-async-storage/async-storage"
+
+import AuthContext from "../configs/contexts/AuthContext"
+
+const Login = () => {
+    const LOGIN_URI = "https://easy-login-api.herokuapp.com/users/login"
+
+    const LocalStorage = useAsyncStorage()
+    const { setIsLoggedIn } = useContext(AuthContext)
+
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    const login = async () => {
+        const query = await axios.post(LOGIN_URI, {
+            username: username,
+            password: password,
+        })
+
+        if (query.headers["x-access-token"]) {
+            await LocalStorage.setItem("token", query.headers["x-access-token"])
+            await setIsLoggedIn(true)
+        }
+    }
+
+    return (
+        <SafeAreaView>
+            <TextInput placeholder="Username" value={username} onChangeText={(e) => setUsername(e)} />
+            <TextInput placeholder="Password" value={password} onChangeText={(e) => setPassword(e)} secureTextEntry />
+            <Button title="Connexion" onPress={login} />
+        </SafeAreaView>
+    )
+}
+
+export default Login
