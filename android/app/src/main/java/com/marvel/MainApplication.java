eb748211d3e7eee10aa736193debc4eeee514ce5
ecandotti <enzo.candotti16@hotmail.com>
2022-03-06 19:45:32 +0100

    react native config

--- android/app/src/main/java/com/marvel/MainApplication.java
+++ android/app/src/main/java/com/marvel/MainApplication.java
@@ -7,6 +7,9 @@ import androidx.annotation.NonNull;
 
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.netinfo.NetInfoPackage;
+import com.reactnativecommunity.netinfo.NetInfoPackage;
+import com.reactnativecommunity.netinfo.NetInfoPackage;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
