56736a9274bdaaaa6642a59989ee2fbbdc819d7d
ecandotti <enzo.candotti16@hotmail.com>
2022-03-07 01:34:01 +0100

    add search feature

--- src/screens/selfcare/Characters.jsx
+++ src/screens/selfcare/Characters.jsx
@@ -1,59 +1,75 @@
 import React, { useContext, useEffect, useState } from "react"
-import { SafeAreaView, Button, FlatList, Text } from "react-native"
+import { SafeAreaView, FlatList, Text, Button } from "react-native"
+import Fuse from "fuse.js"
 
 import api from "../../utils/api"
 
 import apiKey from "../../configs/api/apiKey"
-import AuthContext from "../../configs/contexts/AuthContext"
 import MarvelContext from "../../configs/contexts/MarvelContext"
 
 import Card from "../../components/Card"
+import Search from "../../components/Search"
 
 const Characters = ({ navigation }) => {
-    const { logout } = useContext(AuthContext)
     const { list, setList } = useContext(MarvelContext)
 
+    const [customSearch, setCustomSearch] = useState("")
     const [isLoading, setLoading] = useState(false)
     const [isError, setError] = useState(false)
 
     useEffect(() => {
-        const getAllCharacters = async () => {
-            setLoading(true)
-            setError(false)
-
-            try {
-                const query = await api.get("/v1/public/characters", {
-                    params: {
-                        ...apiKey,
-                    },
-                })
-
-                await setList(query.data.data.results)
-            } catch (error) {
-                setError(true)
-            }
-
-            setLoading(false)
-        }
-
         getAllCharacters()
     }, [])
 
+    const getAllCharacters = async () => {
+        setLoading(true)
+        setError(false)
+
+        try {
+            const query = await api.get("/v1/public/characters", {
+                params: {
+                    ...apiKey,
+                },
+            })
+
+            await setList(query.data.data.results)
+        } catch (error) {
+            setError(true)
+        }
+
+        setLoading(false)
+    }
+
+    const fuse = new Fuse(list, {
+        keys: ["name"],
+    })
+
+    const fuseSearch = fuse.search(customSearch)
+
     return (
         <SafeAreaView>
-            <Button title="Deconnexion" onPress={() => logout()} />
+            <Search customSearch={customSearch} setCustomSearch={setCustomSearch} />
             {isLoading ? (
                 <Text>Fetching data...</Text>
+            ) : fuseSearch.length > 2 ? (
+                <FlatList
+                    data={fuseSearch}
+                    renderItem={({ item }) => <Card item={item.item} navigation={navigation} />}
+                    keyExtractor={(item) => item.item.id}
+                />
             ) : (
-                list && (
-                    <FlatList
-                        data={list}
-                        renderItem={({ item }) => <Card item={item} navigation={navigation} />}
-                        keyExtractor={(item) => item.id}
-                    />
-                )
+                <FlatList
+                    data={list}
+                    renderItem={({ item }) => <Card item={item} navigation={navigation} />}
+                    keyExtractor={(item) => item.id}
+                />
             )}
-            {isError && <Text>Error occured</Text>}
+            {isError && (
+                <>
+                    <Text>Error occured</Text>
+                    <Button title="Réessayer" onPress={() => getAllCharacters()} />
+                </>
+            )}
         </SafeAreaView>
     )
 }
--- src/screens/selfcare/Favorites.jsx
+++ src/screens/selfcare/Favorites.jsx
@@ -1,20 +1,39 @@
-import React, { useContext } from "react"
+import React, { useContext, useState } from "react"
 import { View, FlatList } from "react-native"
+import Fuse from "fuse.js"
 
 import MarvelContext from "../../configs/contexts/MarvelContext"
 
 import Card from "../../components/Card"
+import Search from "../../components/Search"
 
 const Favorites = ({ navigation }) => {
     const { favList } = useContext(MarvelContext)
 
+    const [customSearch, setCustomSearch] = useState("")
+
+    const fuse = new Fuse(favList, {
+        keys: ["name"],
+    })
+
+    const fuseSearch = fuse.search(customSearch)
+
     return (
         <View>
-            <FlatList
-                data={favList}
-                renderItem={({ item }) => <Card item={item} navigation={navigation} />}
-                keyExtractor={(item) => item.id}
-            />
+            <Search customSearch={customSearch} setCustomSearch={setCustomSearch} />
+            {fuseSearch.length > 2 ? (
+                <FlatList
+                    data={fuseSearch}
+                    renderItem={({ item }) => <Card item={item.item} navigation={navigation} />}
+                    keyExtractor={(item) => item.item.id}
+                />
+            ) : (
+                <FlatList
+                    data={favList}
+                    renderItem={({ item }) => <Card item={item} navigation={navigation} />}
+                    keyExtractor={(item) => item.id}
+                />
+            )}
         </View>
     )
 }
