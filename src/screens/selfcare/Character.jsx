11c9667f8821ac745eae61c82773de7fe50d416d
ecandotti <enzo.candotti16@hotmail.com>
2022-03-07 01:33:14 +0100

    refactoring

--- src/screens/selfcare/Character.jsx
+++ src/screens/selfcare/Character.jsx
@@ -1,35 +1,16 @@
-import React, { useEffect, useState } from "react"
+import React from "react"
 import { View, Text, Image } from "react-native"
 
-import api from "../../utils/api"
-import apiKey from "../../configs/api/apiKey"
-
 const Character = ({ route }) => {
-    const { marvelId } = route.params
-
-    const [character, setCharacter] = useState([])
-
-    useEffect(() => {
-        getCharacter()
-    }, [])
-
-    const getCharacter = async () => {
-        const query = await api.get(`/v1/public/characters/${marvelId}`, {
-            params: {
-                ...apiKey,
-            },
-        })
-
-        setCharacter(query.data.data.results[0])
-    }
+    const { character } = route.params
 
     return (
         <View>
-            {/* <Image
-                source={{ uri: `${character.thumbnail.path}.${character.thumbnail.extension}` }}
+            <Image
+                source={{ uri: `${character?.thumbnail.path}.${character?.thumbnail.extension}` }}
                 style={{ width: 100, height: 100 }}
-            /> */}
-            <Text>{character.name}</Text>
+            />
+            <Text>{character?.name}</Text>
         </View>
     )
 }
